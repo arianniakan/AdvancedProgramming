f023c02d3460c628f42c68d6363a097b294cca38
arianniakan <arianniakan1@gmail.com>
2022-11-07 17:31:32 +0330

    first Part Done

--- hw1/Hash.cpp
+++ hw1/Hash.cpp
@@ -1,1 +1,33 @@
-int main
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+int main(){
+    int m;
+    int s;
+    int n;
+    cin >> s >> m;
+    cin >> n;
+    // vector <int> V1;
+    int temp;
+    int result = s;
+    while (n>0){
+        cin>>temp;
+        cout << s << " "<< m << " "<< temp<< " " << n <<"\n";
+
+        result = (s*(result+temp))%m;
+
+        n--;
+    }
+    // for (int i = 0; i<V1.capacity()-1; i++)
+    // {
+    //     s = (s*(s+V1[i]))%m;
+    //     cout << s << " "<< m << " "<< i <<"\n"<<;
+
+    // }
+    cout<<result<<endl;
+    return 0;
+}
